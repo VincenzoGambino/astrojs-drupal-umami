12a3bd32b5ac1a6143a0d9526824c18a56c53039
Vincenzo Gambino <gambinovin@gmail.com>
2024-09-26 16:25:56 +0200

    Contact us React Form.

--- astrojs/astro.config.mjs
+++ astrojs/astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   image: {
     domains: ['drupal-umami.ddev.site'],
     service: {
       entrypoint: 'astro/assets/services/sharp',
       config: {
-        limitInputPixels: false,
-      },
-    },
-  },
+        limitInputPixels: false
+      }
+    }
+  }
 });
